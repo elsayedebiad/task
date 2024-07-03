1442fff7e17a193b264de8e802a332614bcf7565
eng elsayed ebed <engelsayedebiad@gmail.com>
2024-07-04 01:43:05 +0300

    first commit

--- iti_team.dart
+++ iti_team.dart
@@ -6,7 +6,11 @@
  what is the github & gitlab & Azure ? These are all web-based hosting services for Git repositories.
 
  Upload project to gethub ? 
-
+ 1- git init (only one using)
+ 2- git add . (. = all files)
+ using to send file  from local repo (folder) to working area (to can tracking)
+ 3- git commit -m "short massege to know whats the reason upload"
+ using to send file from working area to staging area
  
 
 
