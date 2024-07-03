8ec5337db4334700d9156dcf77c23ec5a4b97127
eng elsayed ebed <engelsayedebiad@gmail.com>
2024-07-04 01:37:52 +0300

    first commit

---
+++ iti_team.dart
@@ -0,0 +1,13 @@
+ /*
+
+
+ what is the  version control ?  It a system thwt tracks  changes  to file  over time . 
+ what is the git ? Git is a distributed version control system .
+ what is the github & gitlab & Azure ? These are all web-based hosting services for Git repositories.
+
+ Upload project to gethub ? 
+
+ 
+
+
+ */
